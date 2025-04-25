ae31e144b3741eec301ec58351d8a300c812bc4a
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 22:56:43 +0300

    qcom: Pass TARGET_KERNEL_VERSION to qtidisplay
    Change-Id: I9c38f13a0341bd8a234550e5488b3a225c87c350
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -126,6 +126,7 @@ SOONG_CONFIG_qtidisplay += \
     smmu_proxy \
     ubwcp_headers \
     wide_color \
+    target_kernel_version \
     target_no_raw10_custom_format \
     target_uses_aligned_ycbcr_height \
     target_uses_aligned_ycrcb_height \
@@ -155,6 +156,7 @@ SOONG_CONFIG_qtidisplay_smmu_proxy ?= false
 SOONG_CONFIG_qtidisplay_ubwcp_headers ?= false
 SOONG_CONFIG_qtidisplay_composer_version ?= v2
 SOONG_CONFIG_qtidisplay_wide_color ?= false
+SOONG_CONFIG_qtidisplay_target_kernel_version ?= 0
 SOONG_CONFIG_qtidisplay_target_no_raw10_custom_format ?= false
 SOONG_CONFIG_qtidisplay_target_uses_aligned_ycbcr_height ?= false
 SOONG_CONFIG_qtidisplay_target_uses_aligned_ycrcb_height ?= false
@@ -179,6 +181,10 @@ endif
 
 ifeq ($(TARGET_USES_FOD_ZPOS),true)
     SOONG_CONFIG_qtidisplay_udfps := true
+endif
+
+ifneq ($(TARGET_KERNEL_VERSION),)
+    SOONG_CONFIG_qtidisplay_target_kernel_version := $(TARGET_KERNEL_VERSION)
 endif
 
 # For libgrallocutils features
